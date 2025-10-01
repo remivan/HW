4e7224480061d65defeff089b80d82801cd5c3d4
Irina <dead.rescuer@yandex.com>
2025-10-01 22:28:08 +0300

    changed

--- src/main/java/com/proni384/Main.java
+++ src/main/java/com/proni384/Main.java
@@ -3,7 +3,7 @@ package com.proni384;
 
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Hello");
+            System.out.println("qqqqqqqqqqqqqqqq");
 
     }
 }
